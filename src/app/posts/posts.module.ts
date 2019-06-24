3809d3a33262289e7185bc2f9acd6375e1e39388
yani-valeva <yani.valeva@gmail.com>
2019-06-24 15:59:23 +0300

    Create view and edit component

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index/index.component';
 import { PostsRoutingModule } from './posts-routing.module';
+import { ViewComponent } from './view/view.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
-  declarations: [IndexComponent],
+  declarations: [IndexComponent, ViewComponent, EditComponent],
   imports: [
     CommonModule,
     PostsRoutingModule
